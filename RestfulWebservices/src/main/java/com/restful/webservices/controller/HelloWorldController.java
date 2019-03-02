13e4271f5a5724c3bd616bb3bc294502815570ed
surendra <surendra@192.168.1.3>
2019-03-02 08:12:46 +0530

    Internationalization

--- RestfulWebservices/src/main/java/com/restful/webservices/controller/HelloWorldController.java
+++ RestfulWebservices/src/main/java/com/restful/webservices/controller/HelloWorldController.java
@@ -1,13 +1,21 @@
 package com.restful.webservices.controller;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.restful.webservices.beans.HelloWorldBean;
 
 @RestController
 public class HelloWorldController {
+	
+	@Autowired
+	private MessageSource messageSource; 
 	
 	@GetMapping(path="/hello-world")
 	public String helloWorld() {
@@ -25,5 +33,9 @@ public class HelloWorldController {
 		return new  HelloWorldBean(String.format("Hello, %s", name));
 	}
 	
+	@GetMapping(path="/hello-world-internationalized")
+	public String helloWorldInternationalized(@RequestHeader(name="Accept-Language", required=false) Locale locale) {
+		return messageSource.getMessage("good.morning.message", null, locale);
+	}
 
 }
